62841026d0574b6d177ecbd255a33a484bfe4377
Ahmed Magaji <magajiahmed98@gmail.com>
2024-12-26 15:13:18 +0100

    new

---
+++ src/components/plate_localizer.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+
+class PlateLocalizer():
+
+    def __init__(self, model_path) -> None:
+        self.model_path = model_path
+    
+    def detect_objects(image):
+        pass
